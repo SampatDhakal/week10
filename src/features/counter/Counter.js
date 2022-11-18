e7b8add176116f74b80f4b31a00c18c57f0998b3
sampatdhakal1999 <sampatdhakal@gmail.com>
2022-11-18 16:57:07 +0545

    moved to redux using toolkit

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,53 +1,68 @@
 import React, { useState } from "react";
 import styles from "./Counter.module.css";
-import { fetchCount } from "./counterAPI";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  selectCount,
+  incrementIfOdd,
+  incrementAsync,
+} from "./counterSlice";
 
 export function Counter() {
+  // const selectCount = (state) => state.counter.value
+
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
+
   /** @TODO move count to reducer and get value to selector */
-  const [count, setCount] = useState(0);
-
-  const [incrementAmount, setIncrementAmount] = useState("2");
-
-  const incrementValue = Number(incrementAmount) || 0;
+  // const [count, setCount] = useState(0);
 
   /** @TODO create required action to decrement redux state value */
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+  // const decrement = () => {
+  //   setCount((prevCount) => prevCount - 1);
+  // };
 
   /** @TODO create required action to increment redux state value */
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+  // const increment = () => {
+  //   setCount((prevCount) => prevCount + 1);
+  // };
 
   /** @TODO create required action to increment by given amount redux state value */
-  const incrementByAmount = (amount) => {
-    setCount((prevCount) => prevCount + +amount);
-  };
+  // const incrementByAmount = (amount) => {
+  //   setCount((prevCount) => prevCount + +amount);
+  // };
+  const [incrementAmount, setIncrementAmount] = useState("2");
+  const incrementValue = Number(incrementAmount) || 0;
+
 
   /** @TODO create required action to increment redux state value if odd by given amount */
-  const incrementIfOdd = (amount) => {
-    /** @TODO implment isOdd condition for the current counter value */
-    const isOdd = false;
-    if (isOdd) {
-      setCount((prevCount) => prevCount + +amount);
-    }
-  };
+  // const incrementIfOdd = (amount) => {
+  //   /** @TODO implment isOdd condition for the current counter value */
+  //   const isOdd = false;
+  //   if (isOdd) {
+  //     setCount((prevCount) => prevCount + +amount);
+  //   }
+  // };
 
-  /**
-   * @TODO create required action to increment redux state value by fetching value
-   * from {@link fetchCount} async function
-   * Also handle loading states to disable other buttons while the data is being fetched
-   * */
-  const incrementAsync = async (amount) => {
-    const response = await fetchCount(amount);
-    const amountFromAsyncFunction = response.data;
+  // /**
+  // *@TODO create required action to increment redux state value by fetching value
+  //  * from {@link fetchCount} async function
+  //  * Also handle loading states to disable other buttons while the data is being fetched
+  //  * */
+  // const incrementAsync = async (amount) => {
+  //   const response = await fetchCount(amount);
+  //   const amountFromAsyncFunction = response.data;
 
-    setCount((prevCount) => prevCount + +amountFromAsyncFunction);
-  };
+  //   setCount((prevCount) => prevCount + +amountFromAsyncFunction);
+  // };
 
   /** @TODO Disable all the buttons while counter is being incremented asynchronously */
-  const isAsyncActionInProcess = false;
+  // const isAsyncActionInProcess = false;
+  const [isAsyncActionInProcess, setIsAsyncActionInProcess] = useState(false);
 
   return (
     <div>
@@ -56,7 +71,7 @@ export function Counter() {
           className={styles.button}
           disabled={isAsyncActionInProcess}
           aria-label="Decrement value"
-          onClick={decrement}
+          onClick={() => dispatch(decrement())}
         >
           -
         </button>
@@ -65,11 +80,12 @@ export function Counter() {
           className={styles.button}
           disabled={isAsyncActionInProcess}
           aria-label="Increment value"
-          onClick={increment}
+          onClick={() => dispatch(increment())}
         >
           +
         </button>
       </div>
+
       <div className={styles.row}>
         <input
           className={styles.textbox}
@@ -80,21 +96,26 @@ export function Counter() {
         <button
           className={styles.button}
           disabled={isAsyncActionInProcess}
-          onClick={() => incrementByAmount(incrementValue)}
+          onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
           className={styles.asyncButton}
           disabled={isAsyncActionInProcess}
-          onClick={() => incrementAsync(incrementValue)}
+          onClick={() => {
+            setIsAsyncActionInProcess(true);
+            dispatch(incrementAsync(incrementValue)).then(() => {
+              setIsAsyncActionInProcess(false);
+            }).catch(() => setIsAsyncActionInProcess(false));
+          }}
         >
           Add Async
         </button>
         <button
           className={styles.button}
           disabled={isAsyncActionInProcess}
-          onClick={() => incrementIfOdd(incrementValue)}
+          onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
           Add If Odd
         </button>
